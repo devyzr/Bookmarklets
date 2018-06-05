ed3c3be584ae34b4bf79da0b1ca95fa1f7e05098
Devyzr <daniel.e.verdin@gmail.com>
2018-06-05 17:38:37 -0500

    Now remove the clap thing from medium

--- Hide headers and footers medium.js
+++ Hide headers and footers medium.js
@@ -2,11 +2,14 @@ javascript:(
     function(){
         header_selectors = "div > div.metabar.u-clearfix.js-metabar.u-fixed.u-backgroundTransparentWhiteDarkest.u-xs-sizeFullViewportWidth";
         footer_selectors = "div > div.u-fixed.u-bottom0.u-sizeFullWidth.u-backgroundWhite.u-boxShadowTop.u-borderBox.u-paddingTop10.u-paddingBottom10.u-zIndexMetabar.u-xs-paddingLeft10.u-xs-paddingRight10.js-stickyFooter";
+        clapper_thing = "div > aside > div > ul";
 
         header_elemnt = document.querySelector(header_selectors);
         footer_element = document.querySelector(footer_selectors);
+        clapper_element = document.querySelector(clapper_thing);
 
         header_elemnt.style.display = "none";
         footer_element.style.display = "none";
+        clapper_element.style.display = "none";
     }()
 );
